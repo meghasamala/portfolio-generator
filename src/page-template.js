4672ccf7c95f69eed95095ee13dea7693096f9d1
megha <msamala21@gmail.com>
2021-09-27 03:57:34 -0500

    added Promises to generate site

--- src/page-template.js
+++ src/page-template.js
@@ -27,7 +27,7 @@ const generateProjects = projectsArr => {
                 ${languages.join(', ')}
               </h5>
               <p>${description}</p>
-              <a href="${link}" class="btn"><i class="fab fa-github mr-2"></i>View Project on GitHub</a>
+              <a href="https://github.com/${link}" class="btn"><i class="fab fa-github mr-2"></i>View Project on GitHub</a>
             </div>
           `;
           })
@@ -44,7 +44,7 @@ const generateProjects = projectsArr => {
                 ${languages.join(', ')}
               </h5>
               <p>${description}</p>
-              <a href="${link}" class="btn mt-auto"><i class="fab fa-github mr-2"></i>View Project on GitHub</a>
+              <a href="https://github.com/${link}" class="btn mt-auto"><i class="fab fa-github mr-2"></i>View Project on GitHub</a>
             </div>
           `;
           })
